7d8367174d6b90255a0c592f5cdf9a151adbdf82
prajapati kiran <prajapatikiran5022gmail.com>
2023-12-22 14:35:33 +0530

    new project added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './Components/ListEmployeeComponent'
+function App() {
+  return (
+    <div className="container">
+      			hello  world
+    </div>
+  );
+}
+
+export default App;
